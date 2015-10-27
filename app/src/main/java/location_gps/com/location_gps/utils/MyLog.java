e501b26220d460ebd5b540af96227a548dc29ef5
dev-bpn <jazzbpn@gmail.com>
2015-10-27 10:27:01 +0545

    Create log file

---
+++ app/src/main/java/location_gps/com/location_gps/utils/MyLog.java
@@ -0,0 +1,13 @@
+package location_gps.com.location_gps.utils;
+
+import android.util.Log;
+
+/**
+ * Created by Dell on 10/27/2015.
+ */
+public class MyLog {
+
+    public static void shoeLog(String message){
+        Log.i("MY_TAG" , message);
+    }
+}
